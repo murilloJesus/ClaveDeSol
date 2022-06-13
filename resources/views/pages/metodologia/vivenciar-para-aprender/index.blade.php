687a8f34239ab54a9fa32a736700e5af9af2cf5b
Leon14789 <104097806+Leon14789@users.noreply.github.com>
2022-06-13 15:51:21 -0300

    Atualizações CSS
    atualizações dos Cards

--- resources/views/pages/metodologia/vivenciar-para-aprender/index.blade.php
+++ resources/views/pages/metodologia/vivenciar-para-aprender/index.blade.php
@@ -13,8 +13,9 @@
             <div class="image-wrapper first">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                 <h3>Vivenciar para Aprender Metodologia de Ensino Clave de Sol</h3>
             <p align="center">
-                As atividades que envolvem corpo e movimento favorecer o desenvolvimento global, que introduz e integra o aluno na cultura corporal de movimento e de uma forma...
+                As atividades que envolvem corpo e movimento favorecer o desenvolvimento global, que introduz e...
             </p>
             <ul class="actions major">
                 <li><a href="/metodologia/vivenciar-para-aprender/Metodologia-de-Ensino-Pedagogico-do-Instituto-de-E/" class="button">Veja Mais</a></li>
@@ -25,6 +26,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                 <h3> Boas Vindas Ação E Organização PTS E PT1 Ação E Organização </h3>
             <p align="center">
                Objetivo: O ano de 2019 acaba de iniciar, e agora é tudo novo de novo! Vamos acolher cada um que...
             </p>
@@ -37,6 +39,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                <h3>Boas Vindas PT2 E PT3 </h3>
             <p align="center">
                 Objetivo: o ano de 2019 acaba de iniciar, e agora é tudo novo de novo! Vamos acolher cada um que chegar...
             </p>
@@ -49,6 +52,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                <h3>Boas vindas Paz a gente que faz PT 4 e PT5 </h3>
             <p align="center">
                Todos que quiserem são bem vindos, e os que não quiserem vão se integrar nessas voltas as aulas... 
             </p>
@@ -61,6 +65,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                <h3>O outro Quem tem um amigo tem um tesouro </h3>
             <p align="center">
                 Objetivo: favorecer a construção da identidade a partir da interação com o outro, e essa interação...
             </p>
@@ -73,6 +78,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                <h3>Reino Animal Como é legal lá no fundo do mar</h3>
             <p align="center">
                existem. Vamos ampliar este conhecimento separando por categorias, sendo assim iniciaremos com os...
             </p>
@@ -85,6 +91,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                <h3>Educação ambiental Preservar o meio ambiente é coisa séria!</h3>
             <p align="center">
                 está em alta, devido a tantas situações de riscos que a nossa natureza encontra-se. Por este motivo...
             </p>
@@ -97,6 +104,7 @@
             <div class="image-wrapper">
                 <a href="#" class="image featured"><img src="/assets/css/images/piscina.png" alt="" /></a>
             </div>
+                <h3>Histórias Carnavalescas </h3>
             <p align="center">
                Tradicionais do mundo e desde pequenas, as crianças, escutam, vivenciam e apreciam a experiência desta... 
             </p>
