0afcc8a9aa6f3d068d716602ec39a09fc3a7f064
Leon14789 <104097806+Leon14789@users.noreply.github.com>
2022-06-28 12:00:03 -0300

    modal
    .

--- resources/views/components/modal-doacoes.blade.php
+++ resources/views/components/modal-doacoes.blade.php
@@ -4,17 +4,22 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Faça uma boa ação</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <small>Dados Bancarios</small> <br>
+                    <small>Agência:</small><br>
+                    <small>Conta:</small> <br>
+                    <small>Instituição Bancaria:</small><br>
+                    <p>Obrigada por contribuir com a nossa causa.<br> Tudo que foi recebido será distribuído e muito bem utilizado por aqueles que mais necessitam.</p>
+
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Voltar</button>
+                    <button type="button" class="btn btn-primary">Realizar uma Doação</button>
                 </div>
             </div>
         </div>
