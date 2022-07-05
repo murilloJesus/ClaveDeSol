e753b2bf55d880a852c7acc5aeab1837e9778a95
Murillo Jesus <murillos.jesus@hotmail.com>
2022-07-05 17:17:40 -0300

    Menu Component Change

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,63 +20,8 @@
 		<div id="page-wrapper">
 
 			<!-- Header -->
-				<div id="header-wrapper">
-					<div id="header" class="container">
 
-						<!-- Logo -->
-							<h1 id="logo"><a href="/">CLAVE DE SOL</a></h1>
-
-						<!-- Nav -->
-							<nav id="nav">
-								<ul>
-									<li>
-										<a href="#">Galerias</a>
-									</li>
-
-									<li>
-                                        <a href="/metodologia/">Metodologia</a>
-										<ul>
-											<li><a href="/metodologia/vivenciar-para-aprender">Vivenciar Para Aprender</a></li>
-											<li><a href="/metodologia/plano-de-trabalho">Plano de Trabalho</a></li>
-											<li><a href="/metodologia/plano-terapeutico">Plano Terapêutico</a></li>
-											<li><a href="#">Atividades Complementares</a>
-                                                <ul>
-                                                    <li><a href="/metodologia/musicalizacao">Musicalização</a></li>
-                                                    <li><a href="/metodologia/educacao-fisica">Educação Fisica</a></li>
-                                                    <li><a href="/metodologia/informatica">Informática</a></li>
-                                                    <li><a href="/metodologia/culinaria">Culinária</a></li>
-                                                    <li><a href="/metodologia/horta">Horta</a></li>
-                                                    <li><a href="/metodologia/piscina">Piscina</a></li>
-                                                    <li><a href="/metodologia/curiosidades/a-importancia-da-educacao-fisica-escolar-no-desenvolvimento-infantil/">Curiosidades</a></li>
-                                                </ul>
-                                            </li>
-
-										</ul>
-									</li>
-
-									<li class="break"><a href="#">Mural</a>
-										<ul>
-											<li><a href="#">Comunicados</a></li>
-											<li><a href="#">Depoimentos</a></li>
-											<li><a href="#">Calendário do Ano</a></li>
-										</ul>
-									</li>
-
-
-									<li><a href="#">Contato</a>
-										<ul>
-											<li><a href="/metodologia/contato/">Contate-nos</a></li>
-											<li><a href="/metodologia/trabalhe">Trabalhe Conosco</a></li>
-										</ul>
-									</li>
-								</ul>
-							</nav>
-
-					</div>
-
-					<!-- Hero -->
-                    @yield('hero')
-				</div>
+            <x-menu></x-menu>
 
                 <div class="wrapper">
 					<div class="container" id="main">
@@ -91,17 +36,17 @@
 				<div id="footer-wrapper">
 					<div id="footer" class="container">
 						<header class="major">
-							<h2>Instituto de Educação Especial Clave de Sol</h2> 
+							<h2>Instituto de Educação Especial Clave de Sol</h2>
 						</header>
 
 							<p id="d"> Contate-nos</p>
 							<div class="tam-footer">
 									<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiK-tre6tr3AhXNjZUCHdHMB5wQFnoECBQQAQ&url=https%3A%2F%2Fpt-br.facebook.com%2FInstitutoClaveDeSol%2F&usg=AOvVaw0eNszAR5MRrietS-2OzQ1Y" target="blank"><img id="footer-img" src="https://cdn.icon-icons.com/icons2/2429/PNG/512/facebook_logo_icon_147291.png" alt="Icone Facebook"></a>
-									
+
 									<a href=""><img id="footer-img" src="https://imagepng.org/wp-content/uploads/2017/08/WhatsApp-icone.png" alt="Icone Whatsapp "> </a>
 									<p id="c"> Nossa metodologia renova completamente o Ensino <br>      visando favorecer a todos os alunos a vivênciar do ensino na prática</p>
 							</div>
-							
+
 					</div>
 
 					<div id="copyright" class="container">
@@ -111,7 +56,7 @@
 					</div>
 				</div>
 
-				
+
 
 
 		</div>
