a1d7bd1045ea8e62d909d0cea2647f7c702d098a
Murillo Jesus <murillos.jesus@hotmail.com>
2022-09-07 08:59:44 -0300

    alterações finais apresentação

--- resources/views/pages/metodologia/vivenciar-para-aprender/index.blade.php
+++ resources/views/pages/metodologia/vivenciar-para-aprender/index.blade.php
@@ -6,7 +6,7 @@
     @endsection
 
 <section class="container">
-  
+
     <div class="row features">
 
         <section class="col-4 col-12-narrower feature">
@@ -27,7 +27,7 @@
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/boas-vindas-acao-e-organizacao/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                 <h3> Boas Vindas Ação E Organização PTS E PT1 Ação E Organização </h3>
+                 <h3> Boas Vindas <br> Ação E Organização  <br> PTS E PT1 </h3>
             <p align="center">
                Objetivo: O ano de 2019 acaba de iniciar, e agora é tudo novo de novo! Vamos acolher cada um que...
             </p>
@@ -40,7 +40,7 @@
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/boas-vindas-pt2-e-pt3/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                <h3>Boas Vindas PT2 E PT3 </h3>
+                <h3>Boas Vindas <br> PT2 E PT3 </h3>
             <p align="center">
                 Objetivo: o ano de 2019 acaba de iniciar, e agora é tudo novo de novo! Vamos acolher cada um que chegar...
             </p>
@@ -48,14 +48,14 @@
                 <li><a href="/metodologia/vivenciar-para-aprender/boas-vindas-pt2-e-pt3/" class="button">Veja Mais</a></li>
             </ul>
         </section>
-        
+
           <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/boas-vindas-paz-a-gente-que-faz-pt 4-ept5/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                <h3>Boas vindas Paz a gente que faz PT 4 e PT5 </h3>
+                <h3>Boas vindas:<br> Paz a gente que faz <br> PT 4 e PT5 </h3>
             <p align="center">
-               Todos que quiserem são bem vindos, e os que não quiserem vão se integrar nessas voltas as aulas... 
+               Todos que quiserem são bem vindos, e os que não quiserem vão se integrar nessas voltas as aulas...
             </p>
             <ul class="actions major">
                 <li><a href="/metodologia/vivenciar-para-aprender/boas-vindas-paz-a-gente-que-faz-pt 4-ept5/" class="button">Veja Mais</a></li>
@@ -66,7 +66,7 @@
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/o-outro-quem-tem-um-amigo-tem-um-tesouro-pts-e-pt1/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                <h3>O outro Quem tem um amigo tem um tesouro </h3>
+                <h3>O outro: <br> Quem tem um amigo tem um tesouro </h3>
             <p align="center">
                 Objetivo: favorecer a construção da identidade a partir da interação com o outro, e essa interação...
             </p>
@@ -74,12 +74,12 @@
                 <li><a href="/metodologia/vivenciar-para-aprender/o-outro-quem-tem-um-amigo-tem-um-tesouro-pts-e-pt1/" class="button">Veja Mais</a></li>
             </ul>
         </section>
-        
+
           <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/como-e-legal-la-no-fundo-do-mar-pt2-e-pt3/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                <h3>Reino Animal Como é legal lá no fundo do mar</h3>
+                <h3>Reino Animal: <br> Como é legal lá no fundo do mar</h3>
             <p align="center">
                existem. Vamos ampliar este conhecimento separando por categorias, sendo assim iniciaremos com os...
             </p>
@@ -87,12 +87,12 @@
                 <li><a href="/metodologia/vivenciar-para-aprender/como-e-legal-la-no-fundo-do-mar-pt2-e-pt3/" class="button">Veja Mais</a></li>
             </ul>
         </section>
-        
+
           <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/preservar-o-meio-ambiente-e-coisa-seria-pt4-e-pt5/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                <h3>Educação ambiental Preservar o meio ambiente é coisa séria!</h3>
+                <h3>Educação ambiental: <br> Preservar o meio ambiente é coisa séria!</h3>
             <p align="center">
                 está em alta, devido a tantas situações de riscos que a nossa natureza encontra-se. Por este motivo...
             </p>
@@ -100,22 +100,22 @@
                 <li><a href="/metodologia/vivenciar-para-aprender/preservar-o-meio-ambiente-e-coisa-seria-pt4-e-pt5/" class="button">Veja Mais</a></li>
             </ul>
         </section>
-        
+
           <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper first">
                 <a href="/metodologia/vivenciar-para-aprender/Historias-carnavalescas-pt4-e-pt5/" class="image featured"><img src="/assets/css/images/books.png" alt="" /></a>
             </div>
-                <h3>Histórias Carnavalescas </h3>
+                <h3>Histórias Carnavalescas</h3>
             <p align="center">
-               Tradicionais do mundo e desde pequenas, as crianças, escutam, vivenciam e apreciam a experiência... 
+               Tradicionais do mundo e desde pequenas, as crianças, escutam, vivenciam e apreciam a experiência...
             </p>
             <ul class="actions major">
                 <li><a href="/metodologia/vivenciar-para-aprender/Historias-carnavalescas-pt4-e-pt5/" class="button">Veja Mais</a></li>
             </ul>
         </section>
-        
-        
-        
+
+
+
     </div>
 </section>
 @endsection
