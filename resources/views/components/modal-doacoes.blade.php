c695d7be2f862f4745c295a9b9b0e002aec1a528
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-08 20:19:12 -0300

    .

--- resources/views/components/modal-doacoes.blade.php
+++ resources/views/components/modal-doacoes.blade.php
@@ -5,6 +5,9 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Faça uma boa ação</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
                     <small>Dados Bancarios</small> <br>
