89de42d1b5656de7eb8c35c25f57e4e37f8b5f33
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-08 20:16:55 -0300

    Alterações Titlle e Modal

--- resources/views/components/modal-doacoes.blade.php
+++ resources/views/components/modal-doacoes.blade.php
@@ -5,21 +5,20 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Faça uma boa ação</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
                     <small>Dados Bancarios</small> <br>
-                    <small>Agência:</small><br>
-                    <small>Conta:</small> <br>
-                    <small>Instituição Bancaria:</small><br>
+                    <small>Agência: 0513</small><br>
+                    <small>Conta: 66046-4</small> <br>
+                    <small>Instituição Bancaria:  Banco Itau </small><br>
+                    <small>PIX: CNPJ - 71558274/0001-49 </small><br>
                     <p>Obrigada por contribuir com a nossa causa.<br> Tudo que foi recebido será distribuído e muito bem utilizado por aqueles que mais necessitam.</p>
 
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="button" data-dismiss="modal">Voltar</button>
-                    <button type="button" class="button">Realizar uma Doação</button>
+                    <button type="button" class="button" data-dismiss="modal">OK</button>
                 </div>
             </div>
         </div>
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Telephasic by HTML5 UP</title>
+		<title>Clave de Sol</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -43,7 +43,7 @@
 							<div class="tam-footer">
 									<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiK-tre6tr3AhXNjZUCHdHMB5wQFnoECBQQAQ&url=https%3A%2F%2Fpt-br.facebook.com%2FInstitutoClaveDeSol%2F&usg=AOvVaw0eNszAR5MRrietS-2OzQ1Y" target="blank"><img id="footer-img" src="https://cdn.icon-icons.com/icons2/2429/PNG/512/facebook_logo_icon_147291.png" alt="Icone Facebook"></a>
 
-									<a href=""><img id="footer-img" src="https://imagepng.org/wp-content/uploads/2017/08/WhatsApp-icone.png" alt="Icone Whatsapp "> </a>
+									<a href="https://api.whatsapp.com/send?phone=5515998394579&text=Ol%C3%A1%20Boa%20tarde"><img id="footer-img" src="https://imagepng.org/wp-content/uploads/2017/08/WhatsApp-icone.png" alt="Icone Whatsapp "> </a>
 									<p id="c"> Nossa metodologia renova completamente o Ensino <br>      visando favorecer a todos os alunos a vivênciar do ensino na prática</p>
 							</div>
 
