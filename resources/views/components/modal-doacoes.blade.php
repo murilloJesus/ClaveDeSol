87b42227e0b132297f459518346cc1f5b5251423
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-08 20:18:06 -0300

    Removendo botao do modal

--- resources/views/components/modal-doacoes.blade.php
+++ resources/views/components/modal-doacoes.blade.php
@@ -5,8 +5,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Faça uma boa ação</h5>
-                        <span aria-hidden="true">&times;</span>
-                    </button>
                 </div>
                 <div class="modal-body">
                     <small>Dados Bancarios</small> <br>
