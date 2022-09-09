237ff8596d170d16a562823e6d17bc4ca9599224
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-09 15:09:39 -0300

    teste5

--- resources/views/pages/metodologia/hora/index.blade.php
+++ resources/views/pages/metodologia/hora/index.blade.php
@@ -1,34 +1,133 @@
-@extends('layouts.front-mixed')
-@section('content')
+<!DOCTYPE HTML>
+<!--
+	Telephasic by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Telephasic by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="right-sidebar is-preload">
+		<div id="page-wrapper">
 
-						<article id="content">
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div id="header" class="container">
 
-								@section('title')
-									<h2 align="center">Piscina</h2>
-								@endsection
-								<p>
-									A piscina tem sido um espaço para os alunos se desenvolverem nas questões da autonomia, autoestima, coordenação motora e um espaço de interação social com os profissionais e entre os próprios alunos.
+						<!-- Logo -->
+							<h1 id="logo"><a href="index.html">Telephasic</a></h1>
 
-								<br>
-									Para participar é necessário a autorização dos pais, são utilizados boias e acompanhamento dos profissionais. A piscina além de ser uma das alternativas de apoio ao trabalho proposto pela Unidade Recanto, é também vista pelos alunos como um momento de lazer e diversão.
-								</br>
-								</p>
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li>
+										<a href="#">Dropdown</a>
+										<ul>
+											<li><a href="#">Lorem ipsum dolor</a></li>
+											<li><a href="#">Magna phasellus</a></li>
+											<li><a href="#">Etiam dolore nisl</a></li>
+											<li>
+												<a href="#">Phasellus consequat</a>
+												<ul>
+													<li><a href="#">Lorem ipsum dolor</a></li>
+													<li><a href="#">Phasellus consequat</a></li>
+													<li><a href="#">Magna phasellus</a></li>
+													<li><a href="#">Etiam dolore nisl</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Veroeros feugiat</a></li>
+										</ul>
+									</li>
+									<li><a href="left-sidebar.html">Left Sidebar</a></li>
+									<li class="break"><a href="right-sidebar.html">Right Sidebar</a></li>
+									<li><a href="no-sidebar.html">No Sidebar</a></li>
+								</ul>
+							</nav>
 
-							</article>
+					</div>
+				</div>
 
+			<!-- Main -->
+				<div class="wrapper">
+					<div class="container" id="main">
+						<div class="row gtr-150">
+							<div class="col-8 col-12-narrower">
+
+								<!-- Content -->
+									<article id="content">
+										<header>
+											<h2>Right Sidebar</h2>
+											<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit
+											dolor neque semper magna lorem ipsum.</p>
+										</header>
+										<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+										<p>Ut sed tortor luctus, gravida nibh eget, volutpat odio. Proin rhoncus, sapien
+										mollis luctus hendrerit, orci dui viverra metus, et cursus nulla mi sed elit. Vestibulum
+										condimentum, mauris a mattis vestibulum, urna mauris cursus lorem, eu fringilla lacus
+										ante non est. Nullam vitae feugiat libero, eu consequat sem. Proin tincidunt neque
+										eros. Duis faucibus blandit ligula, mollis commodo risus sodales at. Sed rutrum et
+										turpis vel blandit. Nullam ornare congue massa, at commodo nunc venenatis varius.
+										Praesent mollis nisi at vestibulum aliquet. Sed sagittis congue urna ac consectetur.</p>
+										<p>Mauris eleifend eleifend felis aliquet ornare. Vestibulum porta velit at elementum
+										gravida nibh eget, volutpat odio. Proin rhoncus, sapien
+										mollis luctus hendrerit, orci dui viverra metus, et cursus nulla mi sed elit. Vestibulum
+										condimentum, mauris a mattis vestibulum, urna mauris cursus lorem, eu fringilla lacus
+										ante non est. Nullam vitae feugiat libero, eu consequat sem. Proin tincidunt neque
+										eros. Duis faucibus blandit ligula, mollis commodo risus sodales at. Sed rutrum et
+										turpis vel blandit. Nullam ornare congue massa, at commodo nunc venenatis varius.
+										Praesent mollis nisi at vestibulum aliquet. Sed sagittis congue urna ac consectetur.</p>
+										<p>Vestibulum pellentesque posuere lorem non aliquam. Mauris eleifend eleifend
+										felis aliquet ornare. Vestibulum porta velit at elementum elementum.</p>
+									</article>
+
+							</div>
+							<div class="col-4 col-12-narrower">
+
+								<!-- Sidebar -->
+									<section id="sidebar">
+										<section>
+											<header>
+												<h3>Elit sed feugiat</h3>
+											</header>
+											<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur et vel
+											sem sit amet dolor neque semper magna. Lorem ipsum dolor sit amet consectetur et dolore
+											adipiscing elit. Curabitur vel sem sit.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">Magna amet nullam</a></li>
+											</ul>
+										</section>
+										<section>
+											<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+											<header>
+												<h3>Commodo lorem varius</h3>
+											</header>
+											<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur et vel
+											sem sit amet dolor neque semper magna. Lorem ipsum dolor sit amet consectetur et dolore
+											adipiscing elit. Curabitur vel sem sit.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">Ipsum sed dolor</a></li>
+											</ul>
+										</section>
+									</section>
+
+							</div>
+						</div>
 						<div class="row features">
 							<section class="col-4 col-12-narrower feature">
 								<div class="image-wrapper first">
 									<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
 								</div>
 								<header>
-									<h3 align="center">
-										Projeto de musicalização (Educador) A Música como Aliada no Desenvolvimento
-									</h3>
+									<h3>Dolor sit consequat magna</h3>
 								</header>
-								<p align="justify">O Instituto visa a experiência dos alunos com a música de forma a englobar aspectos ainda não adquiridos ou até mesmo aprimorar os </p>
+								<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur
+								vel sem sit dolor neque semper magna lorem ipsum.</p>
 								<ul class="actions">
-									<li><a href="/metodologia/musicalizacao/" class="button">Veja Mais</a></li>
+									<li><a href="#" class="button">Elevate my awareness</a></li>
 								</ul>
 							</section>
 							<section class="col-4 col-12-narrower feature">
@@ -36,13 +135,12 @@
 									<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
 								</div>
 								<header>
-									<h3 align="center">Plano de Trabalho de Oficina (PTO) </h3>
+									<h3>Dolor sit consequat magna</h3>
 								</header>
-								<p align="justify">
-									Objetivo esse plano foi desenvolvido para alunos que precisam de autonomia e independência em seus afazeres, que possam ter uma
-								</p>
+								<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur
+								vel sem sit dolor neque semper magna lorem ipsum.</p>
 								<ul class="actions">
-									<li><a href="/metodologia/plano-de-trabalho/plano-de-trabalho-de-oficina-(pto)/" class="button">Veja Mais</a></li>
+									<li><a href="#" class="button">Elevate my awareness</a></li>
 								</ul>
 							</section>
 							<section class="col-4 col-12-narrower feature">
@@ -50,17 +148,80 @@
 									<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
 								</div>
 								<header>
-									<h3 align="center">Plano de Trabalho de 1º ano (PT1) </h3>
+									<h3>Dolor sit consequat magna</h3>
 								</header>
-								<p align="justify">
-									OBJETIVO:Esse Plano de Trabalho foi desenvolvido para alunos que precisam adquirir uma reorganização pessoal, aceitar regras, limites,
-								</p>
+								<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur
+								vel sem sit dolor neque semper magna lorem ipsum.</p>
 								<ul class="actions">
-									<li><a href="/metodologia/plano-de-trabalho/plano-de-trabalho-de-1º-ano-(pt1)/" class="button">Próxima Pagina</a></li>
+									<li><a href="#" class="button">Elevate my awareness</a></li>
 								</ul>
 							</section>
 						</div>
 					</div>
 				</div>
 
-@endsection
+			<!-- Footer -->
+				<div id="footer-wrapper">
+					<div id="footer" class="container">
+						<header class="major">
+							<h2>Euismod aliquam vehicula lorem</h2>
+							<p>Lorem ipsum dolor sit amet consectetur et sed adipiscing elit. Curabitur vel sem sit<br />
+							dolor neque semper magna lorem ipsum feugiat veroeros lorem ipsum dolore.</p>
+						</header>
+						<div class="row">
+							<section class="col-6 col-12-narrower">
+								<form method="post" action="#">
+									<div class="row gtr-50">
+										<div class="col-6 col-12-mobile">
+											<input name="name" placeholder="Name" type="text" />
+										</div>
+										<div class="col-6 col-12-mobile">
+											<input name="email" placeholder="Email" type="text" />
+										</div>
+										<div class="col-12">
+											<textarea name="message" placeholder="Message"></textarea>
+										</div>
+										<div class="col-12">
+											<ul class="actions">
+												<li><input type="submit" value="Send Message" /></li>
+												<li><input type="reset" value="Clear form" /></li>
+											</ul>
+										</div>
+									</div>
+								</form>
+							</section>
+							<section class="col-6 col-12-narrower">
+								<div class="row gtr-0">
+									<ul class="divided icons col-6 col-12-mobile">
+										<li class="icon brands fa-twitter"><a href="#"><span class="extra">twitter.com/</span>untitled</a></li>
+										<li class="icon brands fa-facebook-f"><a href="#"><span class="extra">facebook.com/</span>untitled</a></li>
+										<li class="icon brands fa-dribbble"><a href="#"><span class="extra">dribbble.com/</span>untitled</a></li>
+									</ul>
+									<ul class="divided icons col-6 col-12-mobile">
+										<li class="icon brands fa-instagram"><a href="#"><span class="extra">instagram.com/</span>untitled</a></li>
+										<li class="icon brands fa-youtube"><a href="#"><span class="extra">youtube.com/</span>untitled</a></li>
+										<li class="icon brands fa-pinterest"><a href="#"><span class="extra">pinterest.com/</span>untitled</a></li>
+									</ul>
+								</div>
+							</section>
+						</div>
+					</div>
+					<div id="copyright" class="container">
+						<ul class="menu">
+							<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
