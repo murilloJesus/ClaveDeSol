5dc88587861de36dcbe55a48d1902eb1242f82f7
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-09 14:56:55 -0300

    Menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -34,6 +34,12 @@
 
                     <li class="break"><a href="#">Mural</a>
                         <ul>
+                        <li><a href="#">Projetos</a>
+                    <ul>
+                        <li>Ore Comigo</li>
+                        <li>Hora marcada</li>
+                    </ul>
+                    </li>
                             <li><a href="/metodologia/comunicados/comunicados/">Comunicados</a></li>
                             <li><a href="/metodologia/depoimentos/depoimentos">Depoimentos</a></li>
                             <li><a href="/metodologia/calendario/calendario-escolar">Calendário do Ano</a></li>
