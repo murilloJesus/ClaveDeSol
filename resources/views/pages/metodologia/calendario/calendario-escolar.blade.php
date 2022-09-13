a24b353857536c4338417dd0a61cc4168e33da4e
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-13 15:24:35 -0300

    Calendario Finalizado

--- resources/views/pages/metodologia/calendario/calendario-escolar.blade.php
+++ resources/views/pages/metodologia/calendario/calendario-escolar.blade.php
@@ -12,47 +12,44 @@
 
 			</article>
 
-								<div class="row features">
-									<section class="col-4 col-12-narrower feature">
-										<div class="image-wrapper first">
-											<a href="/metodologia/educacao-fisica/" class="image featured"><img src="/public/assets/css/images/meet.png" alt="" /></a>
-										</div>
-										<header>
-											<h3 align="center">Plano de aula - corpo e movimento - 2021</h3>
-										</header>
-										<p align="justify">As atividades que envolvem corpo e movimento favorecer o desenvolvimento global, que introduz e integra o aluno na cultura corporal de </p>
-										<ul class="actions">
-											<li><a href="/metodologia/educacao-fisica/" class="button">Veja Mais</a></li>
-										</ul>
-									</section>
-									<section class="col-4 col-12-narrower feature">
-										<div class="image-wrapper">
-											<a href="/metodologia/informatica/" class="image featured"><img src="/public/assets/css/images/meet.png" alt="" /></a>
-										</div>
-										<header>
-											<h3>Informatica</h3>
-										</header>
-										<p align="justify">As aulas de informática são realizadas com o objetivo de proporcionar ao aluno o contato com o computador, materiais digitais, além de </p>
-										<ul class="actions">
-											<li><a href="/metodologia/informatica/" class="button">Veja Mais</a></li>
-										</ul>
-									</section>
-									<section class="col-4 col-12-narrower feature">
-										<div class="image-wrapper">
-											<a href="/metodologia/musicalizacao/" class="image featured"><img src="/public/assets/css/images/meet.png" alt="" /></a>
-										</div>
-										<header>
-											<h3 align="center">
-												Projeto de musicalização (Educador) A Música como Aliada no Desenvolvimento
-											</h3>
-										</header>
-										<p align="justify"> O Instituto visa a experiência dos alunos com a música de forma a englobar aspectos ainda não adquiridos ou até mesmo aprimorar os </p>
-										<ul class="actions">
-											<li><a href="/metodologia/musicalizacao/" class="button">Veja Mais</a></li>
-										</ul>
-									</section>
-								</div>
-							</div>
-						</div>
-
+			<div class="row features">
+    <section class="col-4 col-12-narrower feature">
+        <div class="image-wrapper first">
+            <a href="/metodologia/vivenciar-para-aprender" class="image featured"><img src="/public/assets/css/images/books.png" alt="" /></a>
+        </div>
+        <header>
+            <h3 align="center" class="height-2em">
+                Vivenciar para Aprender
+            </h3>
+        </header>
+        <ul class="actions">
+            <li><a href="/metodologia/vivenciar-para-aprender" class="button">Veja Mais</a></li>
+        </ul>
+    </section>
+    <section class="col-4 col-12-narrower feature">
+        <div class="image-wrapper">
+            <a href="/metodologia/plano-de-trabalho" class="image featured"><img src="/public/assets/css/images/meet.png" alt="" /></a>
+        </div>
+        <header>
+            <h3 align="center" class="height-2em">
+                Plano de trabalho
+            </h3>
+        </header>
+        <ul class="actions">
+            <li><a href="/metodologia/plano-de-trabalho" class="button">Veja
+                    Mais</a></li>
+        </ul>
+    </section>
+    <section class="col-4 col-12-narrower feature">
+        <div class="image-wrapper">
+            <a href="/metodologia/plano-terapeutico" class="image featured"><img src="/public/assets/css/images/talking.png" alt="" /></a>
+        </div>
+        <header>
+            <h3 align="center" class="height-2em">Plano Terapeutico</h3>
+        </header>
+        <ul class="actions">
+            <li><a href="/metodologia/plano-terapeutico" class="button">Veja Mais</a></li>
+        </ul>
+    </section>
+</div>
 						@endsection
