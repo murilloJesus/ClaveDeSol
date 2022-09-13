00d169e45a3cd1b9ebd5f20dff68bd8ec73671cf
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-09-13 15:05:12 -0300

    Correção dos Caminhos Metodologia

--- resources/views/pages/metodologia/index.blade.php
+++ resources/views/pages/metodologia/index.blade.php
@@ -8,37 +8,37 @@
     <div class="row features">
         <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper first">
-                <a href="/resources/views/metodologia/vivenciar-para-aprender" class="image featured"><img src="/public/assets/css/images/books.png" alt="" /></a>
+                <a href="/metodologia/vivenciar-para-aprender" class="image featured"><img src="/public/assets/css/images/books.png" alt="" /></a>
             </div>
             <p align="center">
             Essa metodologia, que renova completamente o Ensino desse Instituto, visa favorecertodos os alunos a vivência do ensino na prática, antes da
             </p>
             <ul class="actions major">
-                <li><a href="/resources/views/metodologia/vivenciar-para-aprender" class="button">Veja Mais</a></li>
+                <li><a href="/metodologia/vivenciar-para-aprender" class="button">Veja Mais</a></li>
             </ul>
         </section>
         <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper">
-                <a href="/resources/views/metodologia/piscina" class="image featured"><img src="/public/assets/css/images/pscina.png" alt="" /></a>
+                <a href="/metodologia/piscina" class="image featured"><img src="/public/assets/css/images/pscina.png" alt="" /></a>
             </div>
             <p align="center">
                 A piscina tem sido um espaço para os alunos se desenvolverem nas questões da autonomia, autoestima, coordenação motora e um espaço ...
             </p>
             <ul class="actions major">
-                <li><a href="/resources/views/metodologia/piscina" class="button">Veja Mais</a></li>
+                <li><a href="/metodologia/piscina" class="button">Veja Mais</a></li>
             </ul>
         </section>
         <section class="col-4 col-12-narrower feature">
             <div class="image-wrapper">
-                <a href="/resources/views/metodologia/plano-terapeutico" class="image featured"><img src="/public/assets/css/images/talking.png" alt="" /></a>
+                <a href="/metodologia/plano-terapeutico" class="image featured"><img src="/public/assets/css/images/talking.png" alt="" /></a>
             </div>
             <p align="center">
-                    Dar oportunidade, isso é fundamental para que nossos alunos tenham interesse e consigam adquirir a independência e autonomia que tanto desejamos! Primeiramente é importante.
+                    Dar oportunidade, isso é fundamental para que nossos alunos tenham interesse e consigam adquirir a independência e autonomia que tanto 
             </p>
             <ul class="actions major">
-                <li><a href="/resources/views/metodologia/plano-terapeutico" class="button">Veja Mais</a></li>
+                <li><a href="/metodologia/plano-terapeutico" class="button">Veja Mais</a></li>
             </ul>
-        </section>metodologia/plano-terapeutico
+        </section>
 
     </div>
 </section>
